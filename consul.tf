7ea9c226b94e9eaba390ec0adcb196fdff47dc12
Giau. Tran Minh <giau.tmg@gmail.com>
2021-07-17 04:00:34 +0700

    fix: remove submodule

--- consul.tf
+++ consul.tf
@@ -1,9 +1,15 @@
 resource "helm_release" "consul" {
-  count      = data.terraform_remote_state.cluster.outputs.enable_consul_and_vault ? 1 : 0
-  depends_on = [kubernetes_namespace.secrets]
-  name       = "${var.release_name}-consul"
-  chart      = "${path.module}/consul-helm"
-  namespace  = var.namespace
+  count     = data.terraform_remote_state.cluster.outputs.enable_consul_and_vault ? 1 : 0
+  name      = "${var.release_name}-consul"
+  namespace = var.namespace
+
+  repository = "https://helm.releases.hashicorp.com"
+  chart      = "consul"
+  version    = "0.32.1"
+
+  depends_on = [
+    kubernetes_namespace.secrets
+  ]
 
   set {
     name  = "global.name"
